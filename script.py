43c0394c3d23e26a182355203d2403521e0d228a
7okoma <the0iraqi0king@gmail.com>
2025-04-12 21:12:08 +0300

    Update script.py

--- script.py
+++ script.py
@@ -15,3 +15,37 @@ try:
     )
 except:
     pass
+
+console = Console()
+speed = 0.3  
+colors = ["red", "green", "blue", "yellow", "magenta", "cyan", "white"]
+
+def lightning_effect():
+    console.clear()
+    time.sleep(0.09)  
+
+def animate_text(text, bold=False):
+    for i in range(1, len(text) + 1):
+        console.clear()
+        lightning_effect()
+        partial = text[:i]
+        style = f"bold {random.choice(colors)}" if bold else random.choice(colors)
+        console.print(Text(partial, style=style), justify="center")
+        time.sleep(speed)
+
+def print_large_text(text):
+    banner = text2art(text)  # استخدم `text2art` هنا
+    for _ in range(4):  
+        lightning_effect()
+        console.print(banner, style=f"bold {random.choice(colors)}", justify="center")
+        time.sleep(0.8)
+
+animate_text("only in", bold=False)
+print_large_text("black elgenral")
+
+animate_text("from", bold=False)
+print_large_text("7OKOMA")
+
+print_large_text("bot")
+
+animate_text("Bot is running", bold=False)
